d28eb0f17a54ac859afa8701988aa47b5865100a
Alexey Kotlyarov <a@koterpillar.com>
2021-04-27 20:19:31 +1000

    Logging

--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,8 @@ module Main where
 
 import           Control.Lens
 
+import           Control.Monad  (when)
+
 import           Data.Text      (Text)
 import qualified Data.Text.IO   as Text
 import           Data.Text.Lens (unpacked)
@@ -32,8 +34,20 @@ defaultConfig = Config {_logLevel = Normal, _file = "test.txt"}
 main :: IO ()
 main = act defaultConfig
 
+log_ :: Config -> LogLevel -> Text -> IO ()
+log_ config level message =
+  when (config ^. logLevel >= level) $ Text.putStrLn message
+
+logDebug, logInfo :: Config -> Text -> IO ()
+logDebug config = log_ config Verbose
+
+logInfo config = log_ config Normal
+
 act :: Config -> IO ()
-act config =
+act config = do
+  logDebug config "Starting"
   withFile (config ^. file . unpacked) ReadMode $ \handle -> do
+    logInfo config $ "Catting file: " <> config ^. file
     text <- Text.hGetContents handle
     Text.putStr text
+  logDebug config "Finishing"
