58198a4ac66efd72e3143e7b2e461e8cacc19ac8
Alexey Kotlyarov <a@koterpillar.com>
2021-04-27 20:14:31 +1000

    cat

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,39 @@
+{-# LANGUAGE Haskell2010       #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
+
 module Main where
 
+import           Control.Lens
+
+import           Data.Text      (Text)
+import qualified Data.Text.IO   as Text
+import           Data.Text.Lens (unpacked)
+
+import           System.IO      (IOMode (..), hGetContents, withFile)
+
+data LogLevel
+  = Quiet
+  | Normal
+  | Verbose
+  deriving (Ord, Eq)
+
+data Config =
+  Config
+    { _logLevel :: LogLevel
+    , _file     :: Text
+    }
+
+makeLenses ''Config
+
+defaultConfig :: Config
+defaultConfig = Config {_logLevel = Normal, _file = "test.txt"}
+
 main :: IO ()
-main = print "hello"
+main = act defaultConfig
+
+act :: Config -> IO ()
+act config =
+  withFile (config ^. file . unpacked) ReadMode $ \handle -> do
+    text <- Text.hGetContents handle
+    Text.putStr text
